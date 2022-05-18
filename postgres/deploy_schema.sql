5d8ecdbcf27483a5eac9834dd050e891424469cf
Onix <gotnix13@gmail.com>
2022-05-18 01:21:52 -0400

    Enabled db connections and removed docker seed db data

--- postgres/deploy_schema.sql
+++ postgres/deploy_schema.sql
@@ -1,5 +1,5 @@
--- Deploy fresh DB tables. \i executes sql scripts
-\i '/docker-entrypoint-initdb.d/tables/students.sql'
+-- -- Deploy fresh DB tables. \i executes sql scripts
+-- \i '/docker-entrypoint-initdb.d/tables/students.sql'
 
--- _ Add seed data
-\i '/docker-entrypoint-initdb.d/seed/seed.sql'
+-- -- _ Add seed data
+-- \i '/docker-entrypoint-initdb.d/seed/seed.sql'
